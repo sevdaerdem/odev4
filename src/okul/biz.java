74d2278f65e85ebdc6e3fed6c56f64bf01dcc58b
LENOVO <fkye_vuslat@hotmail.com>
2023-01-30 22:11:54 +0300

    task 1 yapıldı

--- src/okul/biz.java
+++ src/okul/biz.java
@@ -5,6 +5,7 @@ public class biz {
         System.out.println("Sevda Erdem");
         System.out.println("harika bir gün");
         System.out.println("selamlar");
+        System.out.println(" Bugün grup 15 toplandı");
 
 
 
