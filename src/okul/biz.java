f835a49a04dbde48d8e5e1252d1ce6ccac91c277
Casper <sevdaerdem1@windowslive.com>
2023-01-30 22:06:22 +0300

    biz odev yapıyoruz :)   mmmm

--- src/okul/biz.java
+++ src/okul/biz.java
@@ -4,6 +4,9 @@ public class biz {
     public static void main(String[] args) {
         System.out.println("Sevda Erdem");
         System.out.println("harika bir gün");
+        System.out.println("selamlar");
+
+
 
     }
 }
